db81591d120dab77b27c7a50828c6d6f5bbc7f0f
SongHaewon <annto02142374@gmail.com>
2025-10-16 10:49:45 +0900

    Update stopwatch_top.v

--- stopwatch_top.v
+++ stopwatch_top.v
@@ -7,9 +7,9 @@ module stop_watch_top(
     wire toggle;
     wire en;
     reg [2:0] sel;
-    wire tc_100Hz, tc_1MHz;
+    wire tc_1ms, tc_refresh;
 
-    assign en = toggle;
+    assign en = ~toggle;
 
     
     clock_divider U_CLKDIV( clk, reset, en, tc_1ms, tc_refresh );
@@ -31,14 +31,14 @@ end
 
     debounce U_DEBOUNCE (clk, reset, btn, toggle);
 
-    counter_bcd U0 (clk, reset, tc_100Hz, Q0, tc0);
-    counter_bcd U1 (clk, reset, tc0, Q1, tc1);
-    counter_bcd U2 (clk, reset, tc1, Q2, tc2);
-    counter_mod6 U3 (clk, reset, tc2, Q3, tc3);
-    counter_bcd U4 (clk, reset, tc3, Q4, tc4);
-    counter_mod6 U5 (clk, reset, tc4, Q5, tc5);
-    counter_bcd U6 (clk, reset, tc5, Q6, tc6);
-    counter_bcd U7 (clk, reset, tc6, Q7, tc7);
+    counter_bcd U0 (clk, reset, en&tc_1ms, Q0, tc0);
+    counter_bcd U1 (clk, reset, en&tc0, Q1, tc1);
+    counter_bcd U2 (clk, reset, en&tc1, Q2, tc2);
+    counter_mod6 U3 (clk, reset, en&tc2, Q3, tc3);
+    counter_bcd U4 (clk, reset, en&tc3, Q4, tc4);
+    counter_mod6 U5 (clk, reset, en&tc4, Q5, tc5);
+    counter_bcd U6 (clk, reset, en&tc5, Q6, tc6);
+    counter_bcd U7 (clk, reset, en&tc6, Q7, tc7);
 
 
     ss_decoder U_SEG (
@@ -49,12 +49,12 @@ end
 
     always @(*) begin
         case(sel)
-            3'd0: data = Q0;
-            3'd1: data = Q1;
-            3'd2: data = Q2;
-            3'd3: data = Q3;  
-            3'd4: data = Q4;
-            3'd5: data = Q5;  
+            3'd0: data = Q2;
+            3'd1: data = Q3;
+            3'd2: data = Q4;
+            3'd3: data = Q5;  
+            3'd4: data = Q0;
+            3'd5: data = Q1;  
             3'd6: data = Q6;
             3'd7: data = Q7;
             default: data = 4'd0;
